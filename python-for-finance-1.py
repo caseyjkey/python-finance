d702c19cc766f7671d01cd2b4ef3e145e97d3b4f
Casey Key <casey.key@protonmail.com>
2021-08-10 13:06:00 -0700

    finished open high low close graph for Tesla

--- python-for-finance-1.py
+++ python-for-finance-1.py
@@ -1,7 +1,7 @@
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
-import mplfinance
+from mplfinance.original_flavor import candlestick_ohlc
 import matplotlib.dates as mdates
 import pandas as pd
 import pandas_datareader.data as web
@@ -30,5 +30,9 @@ print(df_ohlc.head())
 
 ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
 ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
+ax1.xaxis_date()
 
-#plt.show()
+candlestick_ohlc(ax1, df_ohlc.values, width=2, colorup='g')
+ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values, 0)
+
+plt.show()
